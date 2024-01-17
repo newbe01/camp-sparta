46a11b2b80cb6608c266c7c86b7aaaf31542cd78
newbe01 <pkwaa3@gmail.com>
2024-01-17 17:50:16 +0900

    init

---
+++ spring-prepared/src/main/java/com/sparta/springprepared/SpringPreparedApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springprepared;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPreparedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPreparedApplication.class, args);
+    }
+
+}
---
+++ spring-prepared/src/test/java/com/sparta/springprepared/SpringPreparedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springprepared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPreparedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
