044c51ed186a7d65e1d59c862d05be1e841b4f22
newbe01 <pkwaa3@gmail.com>
2024-01-17 17:40:44 +0900

    init

--- java-basic/src/main/java/week1/Main.java
+++ java-basic/src/main/java/week1/Main.java
@@ -1,4 +1,4 @@
-package camp;
+package week1;
 
 public class Main {
     public static void main(String[] args) {
---
+++ java-basic/src/main/java/week2/operator/W11.java
@@ -0,0 +1,48 @@
+package week2.operator;
+
+public class W11 {
+    public static void main(String[] args) {
+        // if -
+
+        boolean flag = true;
+
+        if (flag) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+
+        // if - else
+        int num = 2;
+        if (num == 1) {
+            System.out.println("num 1");
+        } else if (num == 2) {
+            System.out.println("num 2");
+        } else {
+            System.out.println("else");
+        }
+
+        // 중첩 if
+        if (flag) {
+            System.out.println("true");
+            if (num == 1) {
+                System.out.println("true, 1");
+            } else if (num == 2) {
+                System.out.println("true, 2");
+            } else {
+                System.out.println("true, ??");
+            }
+        } else {
+            System.out.println("false");
+            if (num == 1) {
+                System.out.println("false, 1");
+            } else if (num == 2) {
+                System.out.println("false, 2");
+            } else {
+                System.out.println("false, ??");
+            }
+        }
+
+    }
+}
