736e4a8f65704fec80e3c2dc680e0c30b7257900
newbe01 <pkwaa3@gmail.com>
2024-01-18 12:15:07 +0900

    Jackson

--- spring-mvc/src/main/java/com/sparta/springmvc/response/Star.java
+++ spring-mvc/src/main/java/com/sparta/springmvc/response/Star.java
@@ -3,7 +3,9 @@ package com.sparta.springmvc.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
---
+++ spring-mvc/src/test/java/com/sparta/springmvc/JacksonTest.java
@@ -0,0 +1,33 @@
+package com.sparta.springmvc;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.springmvc.response.Star;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class JacksonTest {
+
+    @Test
+    @DisplayName("Object To JSON : getter 필요")
+    void objectToJson() throws JsonProcessingException {
+        Star star = new Star("Robbie", 95);
+
+        ObjectMapper objectMapper = new ObjectMapper(); // Jackson 라이브러리의 ObjectMapper
+        String json = objectMapper.writeValueAsString(star);
+
+        System.out.println("json = " + json);
+    }
+
+    @Test
+    @DisplayName("JSON To Object : 기본 생성자 & getter, setter 필요")
+    void jsonToObject() throws JsonProcessingException {
+        String json = "{\"name\":\"Robbie\",\"age\":95}"; // JSON 타입의 String
+
+        ObjectMapper objectMapper = new ObjectMapper(); // Jackson 라이브러리의 ObjectMapper
+
+        Star star = objectMapper.readValue(json, Star.class);
+        System.out.println("star = " + star);
+    }
+
+}
