2bc7b554cff0190f74222ee328aaa34e9b19d44c
newbe01 <pkwaa3@gmail.com>
2024-01-18 11:49:25 +0900

    데이터를 Client에 반환하는 방법

---
+++ spring-mvc/src/main/java/com/sparta/springmvc/response/Star.java
@@ -0,0 +1,15 @@
+package com.sparta.springmvc.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Star {
+
+    String name;
+    int age;
+
+}
