90d86afba777ab6d3a20761de3cef1b123a4f6c3
newbe01 <pkwaa3@gmail.com>
2024-01-18 15:48:14 +0900

    update, delete

--- memo/src/main/java/com/sparta/memo/controller/MemoController.java
+++ memo/src/main/java/com/sparta/memo/controller/MemoController.java
@@ -37,4 +37,32 @@ public class MemoController {
         // map to list
         return memoList.values().stream().map(MemoResponseDto::new).toList();
     }
+
+    @PutMapping("memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        // 메모 존재 확인
+        if (memoList.containsKey(id)) {
+            // get memo
+            Memo memo = memoList.get(id);
+
+            // update memo
+            memo.update(requestDto);
+            return memo.getId();
+        } else {
+            throw new IllegalArgumentException("선택한 메모 존재하지 않음");
+        }
+    }
+
+    @DeleteMapping("memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        // 메모 존재 확인
+        if (memoList.containsKey(id)) {
+            // delete memo
+            memoList.remove(id);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모 존재하지 않음");
+        }
+    }
+
 }
