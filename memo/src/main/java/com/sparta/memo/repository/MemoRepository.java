d02afbfe24bf3acc19ab463b09312084ec899bad
newbe01 <pkwaa3@gmail.com>
2024-01-19 21:58:32 +0900

    auditing, query method

--- memo/src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ memo/src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -4,10 +4,14 @@ import com.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 //@Component
 @Repository
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+
+    List<Memo> findAllByOrderByModifiedAtDesc();
+
+    List<Memo> findAllByUsername(String username);
 
 }
