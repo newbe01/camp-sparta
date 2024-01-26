89506c7cd290243b821cef4a5d04f173bb53ddf9
newbe01 <pkwaa3@gmail.com>
2024-01-26 11:19:53 +0900

    rest template get

--- spring-resttemplate-client/src/main/java/com/sparta/springresttemplateclient/dto/ItemDto.java
+++ spring-resttemplate-client/src/main/java/com/sparta/springresttemplateclient/dto/ItemDto.java
@@ -2,6 +2,7 @@ package com.sparta.springresttemplateclient.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.json.JSONObject;
 
 @Getter
 @NoArgsConstructor
@@ -9,5 +10,10 @@ public class ItemDto {
 
     private String title;
     private int price;
+
+    public ItemDto(JSONObject itemJson) {
+        this.title = itemJson.getString("title");
+        this.price = itemJson.getInt("price");
+    }
 
 }
