220a55dde4a2c705aa05968ab10124d3d84a2ae9
newbe01 <pkwaa3@gmail.com>
2024-01-26 10:42:37 +0900

    init

---
+++ spring-resttemplate-client/src/main/java/com/sparta/springresttemplateclient/dto/ItemDto.java
@@ -0,0 +1,13 @@
+package com.sparta.springresttemplateclient.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ItemDto {
+
+    private String title;
+    private int price;
+
+}
---
+++ spring-resttemplate-client/src/main/java/com/sparta/springresttemplateclient/entity/User.java
@@ -0,0 +1,16 @@
+package com.sparta.springresttemplateclient.entity;
+
+import lombok.Getter;
+
+@Getter
+public class User {
+
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+}
