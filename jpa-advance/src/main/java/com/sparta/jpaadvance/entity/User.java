21b34b510f0c5d63292b2f72c3372ec837fe46bd
newbe01 <pkwaa3@gmail.com>
2024-01-29 11:35:13 +0900

    jpa 1:1

---
+++ jpa-advance/src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -0,0 +1,24 @@
+package com.sparta.jpaadvance.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToOne(mappedBy = "user")
+    private Food food;
+
+    public void addFood(Food food) {
+        this.food = food;
+        food.setUser(this);
+    }
+}
