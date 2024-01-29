c8a810362ba5bca5166169ebf93efbe3525a48bd
newbe01 <pkwaa3@gmail.com>
2024-01-29 12:40:42 +0900

    jpa one to many

--- jpa-advance/src/main/java/com/sparta/jpaadvance/entity/Food.java
+++ jpa-advance/src/main/java/com/sparta/jpaadvance/entity/Food.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @Setter
@@ -15,8 +18,8 @@ public class Food {
     private String name;
     private double price;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+    @OneToMany
+    @JoinColumn(name = "food_id") // users 테이블에 food_id 컬럼
+    private List<User> userList = new ArrayList<>();
 
 }
