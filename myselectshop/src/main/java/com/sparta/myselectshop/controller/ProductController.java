e669dbb516bd72f8b67d2b59c2b6e953c0a89d7e
newbe01 <pkwaa3@gmail.com>
2024-01-30 11:26:54 +0900

    관심상품 등록

---
+++ myselectshop/src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.sparta.myselectshop.controller;
+
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
+import com.sparta.myselectshop.dto.ProductRequestDto;
+import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/api")
+@RestController
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping("/products")
+    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
+        return productService.createProduct(requestDto);
+    }
+
+    @PutMapping("/products/{id}")
+    public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
+        return productService.updateProduct(id, requestDto);
+    }
+
+}
